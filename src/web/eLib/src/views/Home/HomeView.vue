1de8d3d264d2563ef454fcb9cd94a8241dc6fa50
Michał <micha.grzelachowski@gmail.com>
2024-11-23 15:22:00 +0100

    feat: testchange

--- src/web/eLib/src/views/Home/HomeView.vue
+++ src/web/eLib/src/views/Home/HomeView.vue
@@ -4,7 +4,7 @@
     <template #content>
       <p class="m-0">
         eLib is a simple library management system. You can add, edit, and
-        delete books. You can also borrow and return books.
+        delete books. You can also borrow and return books. zmiana testowa
       </p>
     </template>
   </Card>
