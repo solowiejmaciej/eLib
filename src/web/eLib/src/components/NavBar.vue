98461fb02799e4df1f11921ba864e6bb7b7669f6
Michał <micha.grzelachowski@gmail.com>
2024-11-24 18:02:13 +0100

    feat: carousel2.0

--- src/web/eLib/src/components/NavBar.vue
+++ src/web/eLib/src/components/NavBar.vue
@@ -1,53 +1,59 @@
-
 <template>
     <div class="card">
-        <Menubar :model="items">
-            
+        <Menubar :model="items" class="custom-menubar">
+            <template #start>
+                <!-- Left items -->
+            </template>
+
             <template #end>
-                <div class="flex flex-nowrap pr-6">
-                    <div><InputText placeholder="Search" type="text" class="w-32 sm:w-auto" /></div>
-                    <div><Menubar :model="[loginItem]"/></div>
+                <div class="end-content">
+                    <div class="search-bar-container">
+                        <InputText placeholder="Search" type="text" class="w-96" />
+                    </div>
+                    <Menubar :model="[loginItem]" />
                 </div>
             </template>
-            
         </Menubar>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import Menubar from 'primevue/menubar';
+import Menubar from "primevue/menubar";
+import InputText from "primevue/inputtext";
+
 const items = ref([
-    {
-        label: 'Home',
-        icon: 'pi pi-home'
-    },
-
-    {
-        label: 'Books',
-        icon: 'pi pi-book'
-    },
-    {
-        label: 'New Arrivals',
-        icon: 'pi pi-sparkles'
-    },
-    {
-        label: 'Bestsellers',
-        icon: 'pi pi-crown'
-    }
+    { label: "Home", icon: "pi pi-home" },
+    { label: "Books", icon: "pi pi-book" },
+    { label: "New Arrivals", icon: "pi pi-sparkles" },
+    { label: "Bestsellers", icon: "pi pi-crown" },
 ]);
-    const loginItem = {
 
-        label: 'Login',
-        icon: 'pi pi-sign-in'
-    };
-
-    
-
+const loginItem = { label: "Login", icon: "pi pi-sign-in" };
 </script>
 
-<script>
+<style>
+.custom-menubar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    padding: 0 1rem;
+}
 
-import Menubar from 'primevue/menubar';
+.search-bar-container {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+}
 
-</script>
+.w-96 {
+    max-width: 24rem;
+}
+.end-content {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+</style>
--- src/web/eLib/src/views/Home/HomeView.vue
+++ src/web/eLib/src/views/Home/HomeView.vue
@@ -2,14 +2,18 @@
   <div>
     <NavBar /> 
   </div>
+  <div>
+    <Gallery />
+  </div>
 </template>
 
 <script>
 import NavBar from '../../components/NavBar.vue';
-
+import Gallery from '../../components/Gallery.vue';
 export default {
   components: {
     NavBar,
+    Gallery,
   },
 };
 </script>
